15fe4c98a3fdcf77386d05a8c10dbdd7ca877828
RanX12 <1726449139@qq.com>
2022-04-09 19:44:11 +0800

    init mblog

---
+++ config/webpack/environment.js
@@ -0,0 +1,52 @@
+const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+
+environment.loaders.prepend('erb', {
+  test: /\.erb$/,
+  enforce: 'pre',
+  exclude: /node_modules/,
+  use: [{
+    loader: 'rails-erb-loader',
+    options: {
+      runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
+    }
+  }]
+})
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', {
+  test: /\.vue$/,
+  use: [{
+    loader: 'vue-loader'
+  }]
+})
+
+environment.loaders.append('css', {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader'],
+})
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+environment.loaders.append('expose', {
+  test: require.resolve('jquery'),
+  use: [{
+    loader: 'expose-loader',
+    options: '$'
+  }, {
+    loader: 'expose-loader',
+    options: 'jQuery',
+  }]
+})
+
+environment.config.merge({
+  devtool: 'none'
+});
+
+module.exports = environment
