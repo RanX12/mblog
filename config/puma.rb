e0df41ea6be0b3a6ad6b362bafba047171ccc831
ranxiao <1726449139@qq.com>
2022-08-28 13:57:56 +0800

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-app_root = '/data/www/mblog/shared'
+app_root = '/home/ruby/data/www/mblog/current'
 pidfile "#{app_root}/tmp/pids/puma.pid"
 state_path "#{app_root}/tmp/pids/puma.state"
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
